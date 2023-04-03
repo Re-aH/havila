f40332e9178bd17bc924cf848b44d95728337961
Re-aH <104552662+Re-aH@users.noreply.github.com>
2023-04-03 15:11:53 +0300

    Add files via upload

---
+++ components/item.js
@@ -0,0 +1,32 @@
+import { TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Item(props) {
+    return (
+        <View style={styles.game}>
+            <TouchableOpacity>
+
+                <Text style={styles.gameText}>
+                    {props.text}
+                </Text>
+            </TouchableOpacity>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    game: {
+        padding: 15,
+        backgroundColor: "#f1f1f1",
+        borderRadius: 10,
+        borderColor: "#C0C0C0",
+        borderWidth: 1,
+        marginTop: 20,
+    },
+    gameText: {
+        flexWrap: 'wrap-reverse',
+        maxWidth: '100%',
+    }
+
+});
